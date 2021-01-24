8b6946896be2d7c393e002f42f11836bde5638a3
Wan <wanwebdev@gmail.com>
2021-01-24 14:59:57 -0500

    Update inner.js
    Completion for inner products, testing & exterior/outer next

--- src/math/PGA/impl/inner.js
+++ src/math/PGA/impl/inner.js
@@ -109,9 +109,9 @@ export const innerPlanePoint = (a, b) => {
 /*
  * Origin ∙ Plane  -> Plane (e1, e2, e3, 0)
  * Origin ∙ Ideal  -> Vanishes completely (assuming pseudo is 0)
- * Origin ∙ Origin -> Origin line (0, 0, 0, s)
- * Origin ∙ Line   -> Origin line (0, 0, 0, s)
- * Origin ∙ Point
+ * Origin ∙ Origin -> Scalar
+ * Origin ∙ Line   -> Scalar
+ * Origin ∙ Point  -> Plane (e1, e2, e3, e0)
 */
 
 /*
@@ -130,7 +130,7 @@ export const innerOriginPlane = (a, b) => {
 };
 
 /*
- * Origin line ∙ Origin line -> Origin line (0, 0, 0, s)
+ * Origin line ∙ Origin line -> Scalar
  * All components vanish except for s, simplifying to:
  * -((a.e23 * b.e23) + (a.e31 * b.e31) + (a.12 * b.e12))
 */
@@ -139,7 +139,7 @@ export const innerOriginOrigin = (a, b) => (
 );
 
 /*
- * Origin line ∙ Line -> Origin line (0, 0, 0, s)
+ * Origin line ∙ Line -> Scalar
  * All components vanish except for s, simplifying to:
  * -((a.e23 * b.e23) + (a.e31 * b.e31) + (a.12 * b.e12))
 */
@@ -149,6 +149,8 @@ export const innerOriginLine = (a, b) => (
 
 /*
  * Origin line ∙ Point -> Plane (e1, e2, e3, e0)
+ * Commutative with Point ∙ Line
+ *
  * Components are the plane that is orthogonal to the line and
  * intersecting the point, simplifying to:
  * -((a.e23 * b.e123)) -> e1
@@ -168,11 +170,11 @@ export const innerOriginPoint = (a, b) => {
 /* === Ideal inner products === */
 
 /*
- * Ideal ∙ Plane
+ * Ideal ∙ Plane  -> Plane (0, 0, 0, e0)
  * Ideal ∙ Ideal  -> Vanishes completely
  * Ideal ∙ Origin -> Vanishes completely (assuming pseudo is 0)
  * Ideal ∙ Line   -> Vanishes completely (assuming pseudo is 0)
- * Ideal ∙ Point
+ * Ideal ∙ Point  -> Vanishes completely (assuming pseudo is 0)
 */
 
 /*
@@ -184,15 +186,167 @@ export const innerIdealPlane = (a, b) => (
   a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
 );
 
+/* === Line inner products === */
+
+/*
+ * Line ∙ Plane  -> Plane(e1, e2, e3, e0)
+ * Line ∙ Ideal  -> Vanishes completely
+ * Line ∙ Origin -> Scalar
+ * Line ∙ Line   -> Scalar
+ * Line ∙ Point  -> Plane(e1, e2, e3, e0)
+*/
+
+/*
+ * Line ∙ Plane -> Plane(e1, e2, e3, e0)
+ * Components are the plane that is orthogonal to the line and
+ * intersecting the plane, simplifying to:
+ * (a.e12 * b.e2 - a.e31 * b.e3) -> e1
+ * (a.e23 * b.e3 - a.e12 * b.e1) -> e2
+ * (a.e31 * b.e1 - a.e23 * b.e2) -> e3
+ * (a.e23 * b.e1) + (a.e31 * b.e2) + (a.e12 * b.e3) -> e0
+*/
+export const innerLinePlane = (a, b) => {
+  const e1 = a[6] * b[1] - a[5] * b[2]; // Offset a by 4 due to origin being
+  const e2 = a[4] * b[2] - a[6] * b[0]; // indices 4-7
+  const e3 = a[5] * b[0] - a[4] * b[1];
+  const e0 = a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+
+  return new Float32Array([e1, e2, e3, e0]);
+};
+
+/*
+ * Line ∙ Origin line -> Scalar
+ * All components vanish except for s, simplifying to:
+ * -((a.e23 * b.e23) + (a.e31 * b.e31) + (a.12 * b.e12))
+*/
+export const innerLineOrigin = (a, b) => (
+  a[4] * b[0] + a[5] * b[1] + a[6] * b[2] // Offset by 4 for indices 4-7
+);
+
+/*
+ * Line ∙ Line -> Scalar
+ * All components vanish except for s, simplifying to:
+ * -((a.e23 * b.e23) + (a.e31 * b.e31) + (a.12 * b.e12))
+*/
+export const innerLineLine = (a, b) => (
+  a[4] * b[0] + a[5] * b[0] + a[6] * b[0] // Offset by 4 for indices 4-7
+);
+
+/*
+ * Line ∙ Point -> Plane (e1, e2, e3, e0)
+ * Commutative with Point ∙ Line
+ *
+ * Components are the plane that is orthogonal to the line and
+ * intersecting the point, simplifying to:
+ * -((a.e23 * b.e123)) -> e1
+ * -((a.e31 * b.e123)) -> e2
+ * -((a.e12 * b.e123)) -> e3
+ * (a.e23 * b.e032) + (a.e31 * b.e013) + (a.e12 * b.e021) -> e0
+*/
+export const innerLinePoint = (a, b) => {
+  const e1 = -(a[4] * b[3]);
+  const e2 = -(a[5] * b[3]);
+  const e3 = -(a[6] * b[3]);
+  const e0 = a[4] * b[0] + a[5] * b[1] + a[6] * b[2];
+
+  return new Float32Array([e1, e2, e3, e0]);
+};
+
 /* === Point inner products === */
 
 /*
- * Point ∙ Plane
- * Point ∙ Ideal
- * Point ∙ Origin
- * Point ∙ Line
- * Point ∙ Point
+ * Point ∙ Plane  -> Line (e01, e02, e03, 0, e23, e31, e12, 0)
+ * Point ∙ Ideal  -> Vanishes completely
+ * Point ∙ Origin -> Plane(e1, e2, e3, e0)
+ * Point ∙ Line   -> Plane(e1, e2, e3, e0)
+ * Point ∙ Point  -> Scalar
 */
+
+/*
+ * Point ∙ Plane -> Ideal line (e01, e02, e03, 0)
+ * Components are the intersections with the ideal plane, simplifying to:
+ * (a.e013 * b.e3 - a.e021 * b.e2) -> e01
+ * (a.e021 * b.e1 - a.e032 * b.e3) -> e02
+ * (a.e032 * b.e2 - a.e013 * b.e1) -> e03
+*/
+export const innerPointPlaneI = (a, b) => {
+  const e01 = a[1] * b[2] - a[2] * b[1];
+  const e02 = a[2] * b[0] - a[0] * b[2];
+  const e03 = a[0] * b[1] - a[1] * b[0];
+
+  return new Float32Array([e01, e02, e03, 0]);
+};
+
+/*
+ * Point ∙ Plane -> Origin line (e23, e31, e12, 0)
+ * Components are the pure Euclidean description of direction, simplifying to:
+ * (a.e123 * b.e1) -> e23
+ * (a.e123 * b.e2) -> e31
+ * (a.e123 * b.e3) -> e12
+*/
+export const innerPointPlaneO = (a, b) => {
+  const e23 = a[3] * b[0];
+  const e31 = a[3] * b[1];
+  const e12 = a[3] * b[2];
+
+  return new Float32Array([e23, e31, e12, 0]);
+};
+
+/*
+ * Point ∙ Plane -> Line (e01, e02, e03, 0, e23, e31, e12, 0)
+ * Combination of the above, full point-plane inner product
+*/
+export const innerPointPlane = (a, b) => {
+  const e01 = a[1] * b[2] - a[2] * b[1];
+  const e02 = a[2] * b[0] - a[0] * b[2];
+  const e03 = a[0] * b[1] - a[1] * b[0];
+
+  const e23 = a[3] * b[0];
+  const e31 = a[3] * b[1];
+  const e12 = a[3] * b[2];
+
+  return new Float32Array([e01, e02, e03, 0, e23, e31, e12, 0]);
+};
+
+/*
+ * Point ∙ Origin line -> Plane (e1, e2, e3, e0)
+ * Commutative with Line ∙ Point
+ *
+ * Components are the plane that is orthogonal to the line and
+ * intersecting the point, simplifying to:
+ * -((a.e123 * b.e23)) -> e1
+ * -((a.e123 * b.e31)) -> e2
+ * -((a.e123 * b.e12)) -> e3
+ * (a.e032 * b.e23) + (a.e013 * b.e31) + (a.e021 * b.e12) -> e0
+*/
+export const innerPointOrigin = (a, b) => {
+  const e1 = -(a[3] * b[0]);
+  const e2 = -(a[3] * b[1]);
+  const e3 = -(a[3] * b[2]);
+  const e0 = a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+
+  return new Float32Array([e1, e2, e3, e0]);
+};
+
+/*
+ * Point ∙ Origin line -> Plane (e1, e2, e3, e0)
+ * Commutative with Line ∙ Point
+ *
+ * Components are the plane that is orthogonal to the line and
+ * intersecting the point, simplifying to:
+ * -((a.e123 * b.e23)) -> e1
+ * -((a.e123 * b.e31)) -> e2
+ * -((a.e123 * b.e12)) -> e3
+ * (a.e032 * b.e23) + (a.e013 * b.e31) + (a.e021 * b.e12) -> e0
+*/
+export const innerPointLine = (a, b) => {
+  const e1 = -(a[3] * b[4]);
+  const e2 = -(a[3] * b[5]);
+  const e3 = -(a[3] * b[6]);
+  const e0 = a[0] * b[4] + a[1] * b[5] + a[2] * b[6];
+
+  return new Float32Array([e1, e2, e3, e0]);
+};
 
 /*
  * Point ∙ Point -> Scalar
