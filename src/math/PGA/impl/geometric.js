5d708bfb02a23e1a27b5c8a72d832d5b128e9e2c
Wan <wanwebdev@gmail.com>
2021-02-14 23:39:36 -0500

    Update geometric.js
    Further updates to geometric products

--- src/math/PGA/impl/geometric.js
+++ src/math/PGA/impl/geometric.js
@@ -165,15 +165,15 @@ export const geometricPlanePoint = (a, b) => {
 
 /* === Ideal line geometric products ===
  *
- * Ideal line * Plane       -> Multivector (e0, e032, e013, e021, e123)
+ * Ideal line * Plane       -> Multivector (e0, e032, e013, e021)
  * Ideal line * Ideal line  -> Vanishes completely
- * Ideal line * Origin line -> Ideal line (e01, e02, e03, e0123)
- * Ideal line * Line        ->
- * Ideal line * Point       ->
+ * Ideal line * Origin line -> Ideal line / Translator (e01, e02, e03, e0123)
+ * Ideal line * Line        -> Ideal line / Translator (e01, e02, e03, e0123)
+ * Ideal line * Point       -> Multivector (e0, e032, e013, e021)
 */
 
 /*
- * Ideal line * Plane -> Multivector (e0, e032, e013, e021, e123)
+ * Ideal line * Plane -> Multivector (e0, e032, e013, e021)
  * l∞ * p = l∞ ∙ p + l∞ ∧ p
  * A valid operation that results in a multivector that is the combination
  * of multiple elements, while simply being equivalent to both the inner and
@@ -191,13 +191,14 @@ export const geometricIdealPlane = (a, b) => {
 };
 
 /*
- * Ideal line * Origin line -> Ideal line (e01, e02, e03, e0123)
+ * Ideal line * Origin line -> Ideal line / Translator (e01, e02, e03, e0123)
  * l∞ * lο = l∞ ∙ lο + l∞ ∧ lο
- * // TODO: Description
- *  -> e01
- *  -> e02
- *  -> e03
- *  -> e0123
+ * Composes the rotation and translation components of an ideal and origin
+ * line, with weighting from both the scalar and pseudoscalar components
+ * (a.e03 * b.e31 - a.e02 * b.e12) - (a.e0123 * b.e23 + a.e01 * b.s) -> e01
+ * (a.e01 * b.e12 - a.e03 * b.e23) - (a.e0123 * b.e31 + a.e02 * b.s) -> e02
+ * (a.e02 * b.e23 - a.e01 * b.e31) - (a.e0123 * b.e12 + a.e03 * b.s) -> e03
+ * (a.e01 * b.e23) + (a.e02 * b.e31) + (a.e03 * b.e12) + (a.e0123 * b.s) -> e0123
 */
 export const geometricIdealOrigin = (a, b) => {
   const e01 = a[2] * b[1] - a[1] * b[2] - a[3] * b[0] + a[0] * b[3];
@@ -208,13 +209,50 @@ export const geometricIdealOrigin = (a, b) => {
   return new Float32Array([e01, e02, e03, e0123]);
 };
 
+/*
+ * Ideal line * Line -> Ideal line / Translator (e01, e02, e03, e0123)
+ * l∞ * ℓ = l∞ ∙ ℓ + l∞ ∧ ℓ
+ * Composes the rotation and translation components of an ideal and full line,
+ * with weighting from both the scalar and pseudoscalar components
+ * (a.e03 * b.e31 - a.e02 * b.e12) - (a.e0123 * b.e23 + a.e01 * b.s) -> e01
+ * (a.e01 * b.e12 - a.e03 * b.e23) - (a.e0123 * b.e31 + a.e02 * b.s) -> e02
+ * (a.e02 * b.e23 - a.e01 * b.e31) - (a.e0123 * b.e12 + a.e03 * b.s) -> e03
+ * (a.e01 * b.e23) + (a.e02 * b.e31) + (a.e03 * b.e12) + (a.e0123 * b.s) -> e0123
+*/
+export const geometricIdealLine = (a, b) => {
+  const e01 = a[2] * b[5] - a[1] * b[6] - a[3] * b[4] + a[0] * b[7];
+  const e02 = a[0] * b[6] - a[2] * b[4] - a[3] * b[5] + a[1] * b[7];
+  const e03 = a[1] * b[4] - a[0] * b[5] - a[3] * b[6] + a[2] * b[7];
+  const e0123 = a[0] * b[4] + a[1] * b[5] + a[2] * b[6] + a[3] * b[7];
+
+  return new Float32Array([e01, e02, e03, e0123]);
+};
+
+/*
+ * Ideal line * Point -> Multivector (e0, e032, e013, e021)
+ * l∞ * P = l∞ ∙ P + l∞ ∧ P
+ * A valid operation that results in a multivector that is the combination
+ * of multiple elements, the equivalent of a point inversely translated by
+ * the ideal without regard to weight. Due to this, it should not be called and
+ * the point should be translated by an appropriate Translator
+*/
+export const geometricIdealPoint = (a, b) => {
+  const e0 = -(a[3] * b[3]);
+
+  const e032 = -(a[0] * b[3]);
+  const e013 = -(a[1] * b[3]);
+  const e021 = -(a[2] * b[3]);
+
+  return new Float32Array([e0, e032, e013, e021]);
+};
+
 /* === Origin line geometric products ===
  *
  * Origin line * Plane       -> Multivector (e1, e2, e3, e0, e032, e013, e021, e123)
- * Origin line * Ideal line  ->
- * Origin line * Origin line ->
- * Origin line * Line        ->
- * Origin line * Point       ->
+ * Origin line * Ideal line  -> Ideal line / Translator (e01, e02, e03, e0123)
+ * Origin line * Origin line -> Rotor (e23, e31, e12, s)
+ * Origin line * Line        -> Motor (e01, e02, e03, e0123, e23, e31, e12, s)
+ * Origin line * Point       -> Multivector (e1, e2, e3, e0, e032, e013, e021, e123)
 */
 
 /*
@@ -239,13 +277,99 @@ export const geometricOriginPlane = (a, b) => {
   return new Float32Array([0, e1, e2, e3, e0, 0, 0, 0, 0, 0, 0, e032, e013, e021, e123, 0]);
 };
 
+/*
+ * Origin line * Ideal line -> Ideal line / Translator (e01, e02, e03, e0123)
+ * lο * l∞ = lο ∙ l∞ + lο ∧ l∞
+ * Composes the rotation and translation components of an origin and ideal
+ * line, with weighting from both the scalar and pseudoscalar components
+ * (a.e12 * b.e02 - a.e31 * b.e03) - (a.s * b.e01 + a.e23 * b.e0123) -> e01
+ * (a.e23 * b.e03 - a.e12 * b.e01) - (a.s * b.e02 + a.e31 * b.e0123) -> e02
+ * (a.e31 * b.e01 - a.e23 * b.e02) - (a.s * b.e03 + a.e12 * b.e0123) -> e03
+ * (a.e23 * b.e01) + (a.e31 * b.e02) + (a.e12 * b.e03) + (a.s * b.e0123) -> e0123
+*/
+export const geometricOriginIdeal = (a, b) => {
+  const e01 = a[2] * b[1] - a[1] * b[2] + a[3] * b[0] - a[0] * b[3];
+  const e02 = a[0] * b[2] - a[2] * b[0] + a[3] * b[1] - a[1] * b[3];
+  const e03 = a[1] * b[0] - a[0] * b[1] + a[3] * b[2] - a[2] * b[3];
+  const e0123 = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3];
+
+  return new Float32Array([e01, e02, e03, e0123]);
+};
+
+/*
+ * Origin line * Origin line -> Rotor (e23, e31, e12, s)
+ * lο * lο = lο ∙ lο + lο ∧ lο
+ * Produces a rotor that, when normalized, moves lο₂ -> lο₁
+ * (a.e12 * b.e31 - a.e31 * b.e12) + (a.e23 * b.s + a.s * b.e23) -> e23
+ * (a.e23 * b.e12 - a.e12 * b.e23) + (a.e31 * b.s + a.s * b.e31) -> e31
+ * (a.e31 * b.e23 - a.e23 * b.e31) + (a.e12 * b.s + a.s * b.e12) -> e12
+ * (a.s * b.s) - (a.e23 * b.e23) - (a.e31 * b.e31) - (a.e12 * b.e12) -> s
+*/
+export const geometricOriginOrigin = (a, b) => {
+  const e23 = a[2] * b[1] - a[1] * b[2] + a[0] * b[3] + a[3] * b[0];
+  const e31 = a[0] * b[2] - a[2] * b[0] + a[1] * b[3] + a[3] * b[1];
+  const e12 = a[1] * b[0] - a[0] * b[1] + a[2] * b[3] + a[3] * b[2];
+  const s = a[3] * b[3] - a[0] * b[0] - a[1] * b[1] - a[2] * b[2];
+
+  return new Float32Array([e23, e31, e12, s]);
+};
+
+/*
+ * Origin line * Line -> Motor (e01, e02, e03, e0123, e23, e31, e12, s)
+ * lο * ℓ = lο ∙ ℓ + lο ∧ ℓ
+ * Produces a motor that, when normalized, moves ℓ -> lο
+ * (a.e12 * b.e02 - a.e31 * b.e03) - (a.s * b.e01 + a.e23 * b.e0123) -> e01
+ * (a.e23 * b.e03 - a.e12 * b.e01) - (a.s * b.e02 + a.e31 * b.e0123) -> e02
+ * (a.e31 * b.e01 - a.e23 * b.e02) - (a.s * b.e03 + a.e12 * b.e0123) -> e03
+ * (a.e23 * b.e01) + (a.e31 * b.e02) + (a.e12 * b.e03) + (a.s * b.e0123) -> e0123
+ * (a.e12 * b.e31 - a.e31 * b.e12) + (a.e23 * b.s + a.s * b.e23) -> e23
+ * (a.e23 * b.e12 - a.e12 * b.e23) + (a.e31 * b.s + a.s * b.e31) -> e31
+ * (a.e31 * b.e23 - a.e23 * b.e31) + (a.e12 * b.s + a.s * b.e12) -> e12
+ * (a.s * b.s) - (a.e23 * b.e23) - (a.e31 * b.e31) - (a.e12 * b.e12) -> s
+*/
+export const geometricOriginLine = (a, b) => {
+  const e01 = a[2] * b[1] - a[1] * b[2] + a[3] * b[0] - a[0] * b[3];
+  const e02 = a[0] * b[2] - a[2] * b[0] + a[3] * b[1] - a[1] * b[3];
+  const e03 = a[1] * b[0] - a[0] * b[1] + a[3] * b[2] - a[2] * b[3];
+  const e0123 = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3];
+
+  const e23 = a[2] * b[5] - a[1] * b[6] + a[0] * b[7] + a[3] * b[4];
+  const e31 = a[0] * b[6] - a[2] * b[4] + a[1] * b[7] + a[3] * b[5];
+  const e12 = a[1] * b[4] - a[0] * b[5] + a[2] * b[7] + a[3] * b[6];
+  const s = a[3] * b[7] - a[0] * b[4] - a[1] * b[5] - a[2] * b[6];
+
+  return new Float32Array([e01, e02, e03, e0123, e23, e31, e12, s]);
+};
+
+/*
+ * Origin line * Point -> Multivector (e1, e2, e3, e0, e032, e013, e021, e123)
+ * lο * P = lο ∙ P + lο ∧ P
+ * A valid operation that results in a multivector that is the combination
+ * of multiple elements, while simply being equivalent to both the inner and
+ * outer products individually. Due to this, it should not be called and
+ * the particular element should be retreived from either product instead.
+*/
+export const geometricOriginPoint = (a, b) => {
+  const e1 = -(a[0] * b[3]);
+  const e2 = -(a[1] * b[3]);
+  const e3 = -(a[2] * b[3]);
+  const e0 = a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+
+  const e032 = a[2] * b[1] - a[1] * b[2] + a[3] * b[0];
+  const e013 = a[0] * b[2] - a[2] * b[0] + a[3] * b[1];
+  const e021 = a[1] * b[0] - a[0] * b[1] + a[3] * b[2];
+  const e123 = a[3] * b[3];
+
+  return new Float32Array([0, e1, e2, e3, e0, 0, 0, 0, 0, 0, 0, e032, e013, e021, e123, 0]);
+};
+
 /* === Line geometric products ===
  *
  * Line * Plane       -> Multivector (e1, e2, e3, e0, e032, e013, e021, e123)
- * Line * Ideal line  ->
- * Line * Origin line ->
+ * Line * Ideal line  -> Ideal line / Translator (e01, e02, e03, e0123)
+ * Line * Origin line -> Motor (e01, e02, e03, e0123, e23, e31, e12, s)
  * Line * Line        ->
- * Line * Point       ->
+ * Line * Point       -> Multivector (e1, e2, e3, e0, e032, e013, e021, e123)
 */
 
 /*
@@ -266,6 +390,74 @@ export const geometricLinePlane = (a, b) => {
   const e013 = a[0] * b[2] - a[2] * b[0] - a[3] * b[1] - a[5] * b[3];
   const e021 = a[1] * b[0] - a[0] * b[1] - a[3] * b[2] - a[6] * b[3];
   const e123 = a[4] * b[0] + a[5] * b[1] + a[6] * b[2];
+
+  return new Float32Array([0, e1, e2, e3, e0, 0, 0, 0, 0, 0, 0, e032, e013, e021, e123, 0]);
+};
+
+/*
+ * Line * Ideal line -> Ideal line / Translator (e01, e02, e03, e0123)
+ * ℓ * l∞ = ℓ ∙ l∞ + ℓ ∧ l∞
+ * Composes the rotation and translation components of a line and an ideal,
+ * with weighting from both the scalar and pseudoscalar components
+ * (a.e12 * b.e02 - a.e31 * b.e03) - (a.e23 * b.e0123 + a.s * b.e01) -> e01
+ * (a.e23 * b.e03 - a.e12 * b.e01) - (a.e31 * b.e0123 + a.s * b.e02) -> e02
+ * (a.e31 * b.e01 - a.e23 * b.e02) - (a.e12 * b.e0123 + a.s * b.e03) -> e03
+ * (a.e23 * b.e01) + (a.e31 * b.e02) + (a.e12 * b.e03) + (a.s * b.e0123) -> e0123
+*/
+export const geometricLineIdeal = (a, b) => {
+  const e01 = a[6] * b[1] - a[5] * b[2] - a[4] * b[3] + a[7] * b[0];
+  const e02 = a[4] * b[2] - a[6] * b[0] - a[5] * b[3] + a[7] * b[1];
+  const e03 = a[5] * b[0] - a[4] * b[1] - a[6] * b[3] + a[7] * b[2];
+  const e0123 = a[4] * b[0] + a[5] * b[1] + a[6] * b[2] + a[7] * b[3];
+
+  return new Float32Array([e01, e02, e03, e0123]);
+};
+
+/*
+ * Line * Origin line -> Motor (e01, e02, e03, e0123, e23, e31, e12, s)
+ * ℓ * lο = ℓ ∙ lο + ℓ ∧ lο
+ * Produces a motor that, when normalized, moves lο -> ℓ
+ * (a.e03 * b.e31 - a.e02 * b.e12) - (a.e0123 * b.e23 + a.e01 * b.s) -> e01
+ * (a.e01 * b.e12 - a.e03 * b.e23) - (a.e0123 * b.e31 + a.e02 * b.s) -> e02
+ * (a.e02 * b.e23 - a.e01 * b.e31) - (a.e0123 * b.e12 + a.e03 * b.s) -> e03
+ * (a.e01 * b.e23) + (a.e02 * b.e31) + (a.e03 * b.e12) + (a.e0123 * b.s) -> e0123
+ * (a.e12 * b.e31 - a.e31 * b.e12) + (a.e23 * b.s + a.s * b.e23) -> e23
+ * (a.e23 * b.e12 - a.e12 * b.e23) + (a.e31 * b.s + a.s * b.e31) -> e31
+ * (a.e31 * b.e23 - a.e23 * b.e31) + (a.e12 * b.s + a.s * b.e12) -> e12
+ * (a.s * b.s) - (a.e23 * b.e23) - (a.e31 * b.e31) - (a.e12 * b.e12) -> s
+*/
+export const geometricLineOrigin = (a, b) => {
+  const e01 = a[2] * b[1] - a[1] * b[2] - a[3] * b[0] + a[0] * b[3];
+  const e02 = a[0] * b[2] - a[2] * b[0] - a[3] * b[1] + a[1] * b[3];
+  const e03 = a[1] * b[0] - a[0] * b[1] - a[3] * b[2] + a[2] * b[3];
+  const e0123 = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3];
+
+  const e23 = a[6] * b[1] - a[5] * b[2] + a[4] * b[3] + a[7] * b[0];
+  const e31 = a[4] * b[2] - a[6] * b[0] + a[5] * b[3] + a[7] * b[1];
+  const e12 = a[5] * b[0] - a[4] * b[1] + a[6] * b[3] + a[7] * b[2];
+  const s = a[7] * b[3] - a[4] * b[0] - a[5] * b[1] - a[6] * b[2];
+
+  return new Float32Array([e01, e02, e03, e0123, e23, e31, e12, s]);
+};
+
+/*
+ * Origin line * Point -> Multivector (e1, e2, e3, e0, e032, e013, e021, e123)
+ * ℓ * P = ℓ ∙ P + ℓ ∧ P
+ * A valid operation that results in a multivector that is the combination
+ * of multiple elements, while simply being equivalent to both the inner and
+ * outer products individually. Due to this, it should not be called and
+ * the particular element should be retreived from either product instead.
+*/
+export const geometricLinePoint = (a, b) => {
+  const e1 = -(a[4] * b[3]);
+  const e2 = -(a[5] * b[3]);
+  const e3 = -(a[6] * b[3]);
+  const e0 = a[4] * b[0] + a[5] * b[1] + a[6] * b[2] - a[3] * b[3];
+
+  const e032 = a[6] * b[1] - a[5] * b[2] + a[7] * b[0] - a[0] * b[3];
+  const e013 = a[4] * b[2] - a[6] * b[0] + a[7] * b[1] - a[1] * b[3];
+  const e021 = a[5] * b[0] - a[4] * b[1] + a[7] * b[2] - a[2] * b[3];
+  const e123 = a[7] * b[3];
 
   return new Float32Array([0, e1, e2, e3, e0, 0, 0, 0, 0, 0, 0, e032, e013, e021, e123, 0]);
 };
